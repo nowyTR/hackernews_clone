38064b484498103efe3b8dc6d7aeb200fd41cf49
nowyTR <nowy.inheritance@gmail.com>
2020-02-11 15:04:44 +0100

    added voting option, filter the feed

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -10,6 +10,10 @@ function deletedLinkSubscribe(parent, args, context, info) {
   return context.prisma.$subscribe.link({ mutation_in: ['DELETED'] }).node()
 }
 
+function newVoteSubscribe(parent, args, context, info) {
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+}
+
 const newLink = {
   subscribe: newLinkSubscribe,
   resolve: payload => {
@@ -31,8 +35,16 @@ const deletedLink = {
   }
 }
 
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  }
+}
+
 module.exports = {
   newLink,
   updatedLink,
-  deletedLink
+  deletedLink,
+  newVote
 }
