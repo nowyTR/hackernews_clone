a26336464992e2a37f45ab16a93862ad8a4407f3
nowyTR <nowy.inheritance@gmail.com>
2020-02-14 11:55:24 +0100

    apollo boost migration, setup client manually

--- app/src/index.js
+++ app/src/index.js
@@ -3,28 +3,72 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'emotion-theming'
 import theme from '@rebass/preset'
 import { ApolloProvider } from '@apollo/react-hooks'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { getMainDefinition } from 'apollo-utilities'
+import { split } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { WebSocketLink } from 'apollo-link-ws'
 
 import * as serviceWorker from './serviceWorker'
 import App from './components/App'
-import { API_URL } from './config'
+import { API_URL, WEBSOCKET_URL } from './config'
 import { AUTH_TOKEN } from './constants'
 import './index.css'
 
-const client = new ApolloClient({
-  uri: API_URL,
-  fetchOptions: {
-    credentials: 'include'
-  },
-  request: async operation => {
-    const token = window.localStorage.getItem(AUTH_TOKEN)
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    })
+const httpLink = createHttpLink({
+  uri: API_URL
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
   }
 })
+
+const wsLink = new WebSocketLink({
+  uri: WEBSOCKET_URL,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+// const client = new ApolloClient({
+//   uri: API_URL,
+//   fetchOptions: {
+//     credentials: 'include'
+//   },
+//   request: async operation => {
+//     const token = window.localStorage.getItem(AUTH_TOKEN)
+//     operation.setContext({
+//       headers: {
+//         authorization: `Bearer ${token}`
+//       }
+//     })
+//   }
+// })
 
 ReactDOM.render(
   <ApolloProvider client={client}>
