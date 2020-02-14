aa25e90d270af8ab6a359c335ee16bd7540be054
nowyTR <nowy.inheritance@gmail.com>
2020-02-14 13:25:46 +0100

    add subscriptions

--- app/src/components/LinkList/index.js
+++ app/src/components/LinkList/index.js
@@ -26,8 +26,28 @@ export const FEED_QUERY = gql`
   }
 `
 
-function LinkList() {
-  const { loading, error, data } = useQuery(FEED_QUERY)
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+function LinkListWithData() {
+  const { subscribeToMore, loading, error, data } = useQuery(FEED_QUERY)
 
   const updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
@@ -38,7 +58,45 @@ function LinkList() {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
-  if (loading) {
+  const subscribeToNewLinks = () => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id)
+        if (exists) {
+          return prev
+        }
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  return (
+    <LinkList
+      isLoading={loading}
+      isError={error}
+      links={data?.feed.links}
+      onVote={updateCacheAfterVote}
+      subscribeToNewLinks={subscribeToNewLinks}
+    />
+  )
+}
+
+function LinkList({ isLoading, isError, links, subscribeToNewLinks, onVote }) {
+  React.useEffect(() => {
+    subscribeToNewLinks()
+  }, [])
+
+  if (isLoading) {
     return (
       <Heading fontSize={[5, 6, 7]} color="primary">
         Loading...
@@ -46,7 +104,7 @@ function LinkList() {
     )
   }
 
-  if (error) {
+  if (isError) {
     return (
       <Heading fontSize={[5, 6, 7]} color="primary">
         Error
@@ -56,13 +114,13 @@ function LinkList() {
 
   return (
     <Flex flexWrap="wrap" mx={-2}>
-      {data.feed.links.map(link => (
+      {links.map(link => (
         <Box key={link.id} p={3} width={1 / 2}>
-          <Link link={link} onVote={updateCacheAfterVote} />
+          <Link link={link} onVote={onVote} />
         </Box>
       ))}
     </Flex>
   )
 }
 
-export default LinkList
+export default LinkListWithData
